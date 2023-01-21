a0169f911e3bd0b32f20f635e83594a37f92325e
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-01-21 14:50:23 -0300

    inicio

---
+++ nuevoProyecto.py
@@ -0,0 +1,24 @@
+"""import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+
+window.mainloop()
+"""
+import turtle
+
+window = turtle.Screen()
+
+colors=['red','purple','blue','green','yellow','orange']
+
+t = turtle.Pen()
+t.speed(0)
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
+
+turtle.mainloop()
